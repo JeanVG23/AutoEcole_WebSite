76963fd7fa80b90bfbcef65b8f3d1de8d10be80c
JeanVG23 <156314274+JeanVG23@users.noreply.github.com>
2024-01-11 12:51:28 +0100

    Update connexion.php

--- Auto_Ecole/Pages/connexion.php
+++ Auto_Ecole/Pages/connexion.php
@@ -1,8 +1,8 @@
 <?php
-    $dbhost = 'tuxa.sme.utc';
-    $dbuser = 'nf92a102';
-    $dbpass = 'a1aEu60rnIZQ';
-    $dbname = 'nf92a102';
+    $dbhost = 'tuxa***';
+    $dbuser = 'nf92****';
+    $dbpass = 'a1a*****';
+    $dbname = 'nf92****';
     $connect = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname) or die ('Error connecting to mysql'); //la ligne suivante permet d'éviter les problèmes d'accent entre la page ouèbe et le server mysql
     mysqli_set_charset($connect, 'utf8'); //les données envoyées vers mysql sont encodées en UTF-8
 ?>
